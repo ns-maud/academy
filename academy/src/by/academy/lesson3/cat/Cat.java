32d5b482039b1907c637d520b1acf0f2b9b32251
Vadim <wadim.sidoryk@mail.ru>
2021-01-20 21:17:09 +0300

    add lesson3

--- academy/src/by/academy/lesson3/cat/Cat.java
+++ academy/src/by/academy/lesson3/cat/Cat.java
@@ -11,6 +11,22 @@ public class Cat {
 		super ();
 	}
 	
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
 	public Cat (String nickname) {
 		super ();
 		this.nickname = nickname;
