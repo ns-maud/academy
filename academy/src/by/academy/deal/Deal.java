48f4769df70b3e5b3495adc6e96facd3ae6d3271
ns-maud <nastya.molchanova96@gmail.com>
2021-02-06 01:31:08 +0300

    lesson7+

--- academy/src/by/academy/deal/Deal.java
+++ academy/src/by/academy/deal/Deal.java
@@ -3,38 +3,20 @@ package by.academy.deal;
 public class Deal {
 
 	public static void main(String[] args) {
-		
-		Product Whiskey = new Product ("Jameson", 30, 2, "Ireland");
-		System.out.println(Whiskey.getQuantityAfterPurchase (2));
-		
-		
-		
-		Coffee latte = new Coffee ("Молотый", 1, "Без молока");
+
+		Product Whiskey = new Product("Jameson", 30, 2, "Ireland");
+		System.out.println(Whiskey.getQuantityAfterPurchase(2));
+
+		Coffee latte = new Coffee("Молотый", 1, "Без молока");
 		latte.quantity = 1;
 		latte.manufacturer = "Africa";
-		latte.addSugar (1);
-		
-		Sigarettes kent = new Sigarettes ("С ментолом");
+		latte.addSugar(1);
+
+		Sigarettes kent = new Sigarettes("С ментолом");
 		kent.quantity = 1;
 		kent.getMoodOfTheDay();
 		System.out.println(kent);
-		
-		
-	//	5.	Даны два целых числа A и В. Выведите все числа от A до B включительно, в порядке возрастания, если A < B, или в порядке убывания в противном случае. Использовать рекурсию.
-		
-		int a =1;
-		int b = 10;
-		num (a,b);
-		
+
 	}
-	
-	static void num (int a, int b) {
-		if (a<b) {
-			a++;
-			System.out.println(a);
-			num(a,b);
-			
-		}
-		
-	}
+
 }
